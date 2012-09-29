99ff54f4c58a405a6811149e11a3fd87ff100192
tristen <tristen@tristen.ca>
2012-09-28 20:59:58 -0400

    little fixies

--- js/fonts.js
+++ js/fonts.js
@@ -1,4 +1,15 @@
-(function(context) {
+// http://ejohn.org/apps/jselect/event.html
+function addEvent(object, event, method) {
+    if (object.attachEvent) {
+        object['e' + event + method] = method;
+        object[event + method] = function(){object['e' + event + method](window.event);};
+        object.attachEvent('on' + event, object[event + method]);
+    } else {
+    object.addEventListener(event, method, false);
+    }
+}
+
+!(function(context) {
     var FontUI = function() {};
 
     FontUI.prototype = {
